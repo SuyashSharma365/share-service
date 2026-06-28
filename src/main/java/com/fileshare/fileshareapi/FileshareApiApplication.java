7082e1c7c60fddd5ea010379dda6b673a0d948cd
Suyash Sharma <sharmasuyash198@gmail.com>
2026-06-28 17:07:42 +0530

    Initial Spring Boot project

---
+++ src/main/java/com/fileshare/fileshareapi/FileshareApiApplication.java
@@ -0,0 +1,13 @@
+package com.fileshare.fileshareapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileshareApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileshareApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fileshare/fileshareapi/FileshareApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fileshare.fileshareapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileshareApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
