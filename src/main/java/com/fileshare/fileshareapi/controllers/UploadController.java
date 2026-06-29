742f00cba05752817107c77883f965835975d79f
Suyash Sharma <sharmasuyash198@gmail.com>
2026-06-29 21:08:14 +0530

    S3 service created to done upload

--- src/main/java/com/fileshare/fileshareapi/controllers/UploadController.java
+++ src/main/java/com/fileshare/fileshareapi/controllers/UploadController.java
@@ -1,16 +1,31 @@
 package com.fileshare.fileshareapi.controllers;
 
 
+import com.fileshare.fileshareapi.service.S3Service;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+import java.io.IOException;
 
-@RestController("/v1/upload")
+
+@RestController()
+@RequiredArgsConstructor
 public class UploadController {
 
-    @PostMapping
-    public ResponseEntity<null> upload(){
-        return ResponseEntity<null>;
+    private final S3Service s3Service;
+
+    @PostMapping("/v1/upload")
+    public ResponseEntity<Boolean> upload(@RequestParam("file") MultipartFile multipartFile) throws IOException {
+        try{
+            s3Service.upload(multipartFile);
+            return ResponseEntity.ok(true);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
     }
 
 }
---
+++ src/main/java/com/fileshare/fileshareapi/service/S3Service.java
@@ -0,0 +1,29 @@
+package com.fileshare.fileshareapi.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+import software.amazon.awssdk.core.sync.RequestBody;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+
+import java.io.IOException;
+
+@Service
+@RequiredArgsConstructor
+public class S3Service {
+
+    private final  S3Client s3Client;
+
+    @Value("${aws.bucket.name}")
+    private String bucketName;
+
+    public void upload(MultipartFile multipartFile) throws IOException{
+        PutObjectRequest fileObj = PutObjectRequest.builder()
+                .bucket(bucketName)
+                .key(multipartFile.getOriginalFilename())
+                .build();
+        s3Client.putObject(fileObj , RequestBody.fromBytes(multipartFile.getBytes()));
+    }
+}
