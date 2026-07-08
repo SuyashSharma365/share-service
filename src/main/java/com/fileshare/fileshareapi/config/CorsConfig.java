e1af85615a7f955f8e85073f5d1d2226f47978f0
Suyash Sharma <sharmasuyash198@gmail.com>
2026-07-08 22:45:00 +0530

    change the port

--- src/main/java/com/fileshare/fileshareapi/config/CorsConfig.java
+++ src/main/java/com/fileshare/fileshareapi/config/CorsConfig.java
@@ -13,7 +13,7 @@ public class CorsConfig implements WebMvcConfigurer{
 
         registry.addMapping("/**")
                 .allowedOrigins(
-                        "http://localhost:5173",
+                        "http://localhost:3000",
                         "https://dropcode.space",
                         "https://www.dropcode.space"
                 )
